b960b0a8ec7c11873c1672b91b08d1d4085aaa2e
Gabriel Lebis <Gabriel.Lebis@genesys.com>
2022-05-09 10:36:27 +0200

    add more dynamic parsing

--- src/converter.ts
+++ src/converter.ts
@@ -7,9 +7,17 @@ const timeFormat = new Intl.DateTimeFormat("fr-FR", {
   timeStyle: "short",
 });
 
+function parseTime(toParse: string, tz: string) {
+  if (toParse.toLowerCase().includes("m")) {
+    return moment.tz(toParse, "hha", tz);
+  } else {
+    return moment.tz(toParse, "HH:mm", tz);
+  }
+}
+
 // console.log(process.argv);
 if (process.argv.length !== 4) {
-  console.error("Erro : wrong number of arguments");
+  console.error("Error : wrong number of arguments");
   process.exit(1);
 }
 
@@ -17,8 +25,8 @@ const [range, tz] = process.argv.splice(2);
 const [startStr, endStr] = range.split("-");
 
 // 11pm-4am ET
-const startDate = moment.tz(startStr, "hha", tz);
-const endDate = moment.tz(endStr, "hha", tz);
+const startDate = parseTime(startStr, tz);
+const endDate = parseTime(endStr, tz);
 
 const formattedStart = timeFormat.format(startDate.toDate());
 const formattedEnd = timeFormat.format(endDate.toDate());
