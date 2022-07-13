9ce3378f2b5bee9724d40cda266c731f2d2d8d3e
Gabriel Lebis <Gabriel.Lebis@genesys.com>
2022-07-13 15:42:04 +0200

    add usage info

--- src/converter.ts
+++ src/converter.ts
@@ -1,5 +1,11 @@
 import { convert } from "./lib";
 
 const args = process.argv.splice(2);
+
+if (args.length < 2) {
+  console.log("Usage : node converter.js <startTime> <endTime?> <timezone>");
+  process.exit(0);
+}
+
 const tz = args.pop() as string;
 console.log(convert(args.join(" "), tz));
